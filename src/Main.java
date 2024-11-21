b0241c5280fa5dc1e7f1889ad065de65773c1044
Alexander-PC <br128128@g,ail.com>
2024-11-21 17:13:49 +0300

    Сток коммит

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+//        int[] weights = new int[]{90, 91, 93, 92, 85, 87, 88, 89, 0, 0, 0, 0};
+//        weights[0] = 90;
+//        int januaryWeigh = weights[0];
+//        System.out.println(januaryWeigh);
+//        System.out.println(weights[0]);
+//        System.out.println(weights[4]);
+//        int january = 0;
+//        System.out.println(weights[january]);
+//        for (int i = 0; i < weights.length; i++) {
+//            System.out.println(weights[i]);
+//        }
+        int[] arr = new int[10];
+        System.out.println(Arrays.toString(arr));
+//        for (int index = 0; index < arr.length; index++) {
+//            if (index == arr.length - 1) {
+//                System.out.println(arr[index]);
+//                break;
+//            }
+//            System.out.print(arr[index] + " ");
+//        }
+    }
+}
+
+
+
+
